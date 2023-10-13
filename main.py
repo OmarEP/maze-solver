b9ac0bba35792ae3d0702a3de53def1cadfcf9e3
OmarEP <omrspz@gmail.com>
2023-10-13 03:30:31 -0400

    Added a gitignore and modified graphics(added point and line class)

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window
+from line import Line 
+from point import Point 
 
+def main():
+    win = Window(800, 600)
+    line = Line(Point(25, 25), Point(123, 200))
+    win.draw_line(line, "red")
+    win.wait_for_close()
+
+main()
